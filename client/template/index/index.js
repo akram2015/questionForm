945724c05e38cba629e517aac9729b8d6673719b
totallytofu <chen33l@mtholyoke.edu>
2016-02-03 16:41:39 -0500

    clear form, insert question to db

--- client/template/index/index.js
+++ client/template/index/index.js
@@ -9,13 +9,22 @@ Template.index.events({
   'click .addQuestion':function(event, template){
     event.preventDefault();
     var text = template.find('#q1').value;
+    $('#q1').val('');
     if (text!= ""){
       var array = Session.get('array');
-      array.push(text);
+      array.push({'question':text, 'answer':null});
       Session.set('array', array);
+
+    }
+
+  },
+
+  'click .finished':function(event, template){
+    if (Session.get ('length') != 0)
+    {
+      Question.insert({filled:false, filledOn: null, QA:Session.get('array'), createAt:new Date()})
+      Session.set('array', []);
     }
 
   }
 })
-//     'click .finished':function(event, template){
-// }
