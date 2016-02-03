a0a888bd177a4a47b5e92377f5f3f65c805a287b
akram <ekrem_engineer@yahoo.com>
2016-02-03 15:57:59 -0500

    question list

--- client/template/index/index.js
+++ client/template/index/index.js
@@ -1,15 +1,21 @@
+Session.set('array',[]);
+Template.index.helpers({
+  question: function(){
+    return Session.get('array');
+  }
+});
 
 Template.index.events({
-  'click .button':function(event, template){
+  'click .addQuestion':function(event, template){
     event.preventDefault();
     var text = template.find('#q1').value;
-    var qa = [{"question": text, "answer":null}];
-    for( var i = 0; i<10; i++){
-      if(text!= ""){
-        Question.insert({filled:false, filledOn: null, QA:qa, createAt:new Date()});
-        console.log(text);
-      }}
+    if (text!= ""){
+      var array = Session.get('array');
+      array.push(text);
+      Session.set('array', array);
+    }
 
-    })
   }
 })
+//     'click .finished':function(event, template){
+// }
