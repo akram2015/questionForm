98187434ed32989b80edec110adf380413213542
totallytofu <chen33l@mtholyoke.edu>
2016-02-03 15:09:53 -0500

    Second commit

--- client/template/index/index.js
+++ client/template/index/index.js
@@ -1,1 +1,15 @@
-Question.insert({filled:false, filledOn: null, QA:[{"question":"Whats your name", "answer":null}], createAt:new Date()});
+
+Template.index.events({
+  'click .button':function(event, template){
+    event.preventDefault();
+    var text = template.find('#q1').value;
+    var qa = [{"question": text, "answer":null}];
+    for( var i = 0; i<10; i++){
+      if(text!= ""){
+        Question.insert({filled:false, filledOn: null, QA:qa, createAt:new Date()});
+        console.log(text);
+      }}
+
+    })
+  }
+})
